7603d3540d3fd72d800c62e358cae9bc87a51095
fangpin <fangpin@bytedance.com>
2019-11-03 22:00:04 +0800

    add coroutine
    Change-Id: Id318a112b2caa2eff8548dbf3774b44c94e9fd20

--- coroutine.h
+++ coroutine.h
@@ -1,12 +1,15 @@
 #ifndef _COROUTINE_H
 #define _COROUTINE_H
 
+#include <iostream>
+#include <functional>
 #include <vector>
-#include <ucontex.h>
+#include <memory>
 
-namespace Fang {
+#include <ucontext.h>
 
 const static int STACK_SIZE = 1024 * 1024 * 2;
+const static int DEFAULT_COROUTINES = 16;
 
 enum CoroutineStatus {
   CO_READY,
@@ -15,26 +18,18 @@ enum CoroutineStatus {
   CO_DEAD,
 };
 
-class Coroutine{
+class Coroutine {
 
 public:
-  template<typename FUNC, typename... ARGS>
-  Coroutine(FUNC&& func, ARGS...&& args)
-    : status_(CO_READY), stackSize_(0) {
-    run_ = [std::forward<FUNC>(func), std::forward<ARGS...>(args)] () {
-            func(...args);
-          };
-  }
-
-  void operator()() {
-    run_();
-  }
-
-    CoroutineStatus getStatus() const {
-        return status_;
-    }
+  void operator()() { run_(); }
+  CoroutineStatus getStatus() const { return status_; }
 
 private:
+  template <typename FUNC, typename... ARGS>
+  Coroutine(FUNC &&func, ARGS &&... args) : status_(CO_READY), stackSize_(0) {
+    run_ = [&]() { func(std::forward<ARGS...>(args...)); };
+  }
+
   std::function<void()> run_;
   std::vector<char> stack_;
   size_t stackSize_;
@@ -45,18 +40,33 @@ private:
 
 class Scheduler {
 public:
-    Scheduler() {
-        stack_.resize(STACK_SIZE);
+  Scheduler() { stack_.resize(STACK_SIZE); }
+
+  size_t running() const {
+    return running_;
+  }
+
+  template <typename FUNC, typename... ARGS>
+  void coroutine(FUNC&& func, ARGS&& ...args) {
+    if (nco_ == coroutines_.size()) {
+      coroutines_.push_back(std::make_shared<Coroutine>(
+                              std::forward<FUNC>(func), std::forward<ARGS...>(args...)));
+    } else {
+      for(auto & it : coroutines_) {
+        if (!it) {
+          it = std::make_shared<Coroutine>(std::foward<FUNC>(func), std::forward<ARGS...>(args...));
+        }
+      }
     }
+    ++nco_;
+  }
+
 private:
   size_t nco_;
   size_t running_;
-  std::vector<Coroutine> coroutines_;
-  vector<char> stack_;
-  ucontex_t main_;
+  std::vector<std::shared_ptr<Coroutine>> coroutines_;
+  std::vector<char> stack_;
+  ucontext_t main_;
 };
-
-} // namespace Fang
-
 
 #endif // #ifndef _COROUTINE_H
